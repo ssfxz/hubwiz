d0fc3ce0e8c705e4d41060eaa9d2a69e5df1b918
LiuY <lyssfxz@126.com>
2017-11-14 16:00:00 +0800

    update es6 note

--- es6/note.mk
+++ es6/note.mk
@@ -110,7 +110,7 @@ Number.isInteger()用来判断一个值是否为整数。
 fill()还可以接受第二个和第三个参数，用于指定填充的起始位置和结束位置。
 ```let newArr = ['a', 'b', 'c'].fill(7, 1, 2) // ['a', 7, 'c']```
 
-### entries() \ keys() \ values()
+### entries() / keys() / values()
 用于遍历数组。它们都返回一个遍历器，可以用for...of循环进行遍历，唯一的区别是keys()是对键名的遍历、values()是对键值的遍历，entries()是对键值对的遍历
 
 ## 对象
@@ -286,4 +286,167 @@ WeakMap结构与Map结构基本类似，唯一的区别是它只接受对象作�
 ## Iterator
 遍历器（Iterator）就是统一的接口机制，来处理所有不同的数据结构。
 
-Iterator的作用有三个：一是为各种数据结构，提供一个统一的、简便的访问接口；二是使得数据结构的成员能够按某种次序排列；三是ES6创造了一种新的遍历命令for...of循环，Iterator接口主要供for...of消费
+Iterator的作用有三个：一是为各种数据结构，提供一个统一的、简便的访问接口；二是使得数据结构的成员能够按某种次序排列；三是ES6创造了一种新的遍历命令for...of循环，Iterator接口主要供for...of消费。
+
+每一次调用next方法，都会返回当前成员的信息，具体来说，就是返回一个包含value和done两个属性的对象。其中，value属性是当前成员的值，done属性是一个布尔值，表示遍历是否结束。
+
+### 遍历器的return()，throw()
+
+### for...of循环
+for...of语句在可迭代对象（包括 Array，Map，Set，String，TypedArray，arguments 对象等等）上创建一个迭代循环，调用自定义迭代钩子，并为每个不同属性的值执行语句。
+
+## Generator
+Generator函数是一个函数的内部状态的遍历器（也就是说，Generator函数是一个状态机）。
+
+形式上，Generator函数是一个普通函数，但是有两个特征。
+
+* 一是，function命令与函数名之间有一个星号；
+* 二是，函数体内部使用yield语句，定义遍历器的每个成员，即不同的内部状态。
+
+```
+function* helloWorldGenerator() {
+  yield 'hello';
+  yield 'world';
+  return 'ending';
+}
+ 
+var hw = helloWorldGenerator();
+hw.next()
+// { value: 'hello', done: false }
+ 
+hw.next()
+// { value: 'world', done: false }
+ 
+hw.next()
+// { value: 'ending', done: true }
+ 
+hw.next()
+// { value: undefined, done: true }
+```
+
+总结一下，调用Generator函数，返回一个部署了Iterator接口的遍历器对象，用来操作内部指针。以后，每次调用遍历器对象的next方法，就会返回一个有着value和done两个属性的对象。value属性表示当前的内部状态的值，是yield语句后面那个表达式的值；done属性是一个布尔值，表示是否遍历结束。
+
+### yield*语句
+　　如果yield命令后面跟的是一个遍历器，需要在yield命令后面加上星号，表明它返回的是一个遍历器。这被称为yield*语句。
+
+　　其实yield关键字就是以一种更直观、便捷的方式让我们创建用于遍历有限序列集合的迭代器，而yield则用于将生成器函数的代码切片作为有限序列集合的元素（元素的类型为指令+数据，而不仅仅是数据而已）
+
+## Promise
+所谓Promise，就是一个对象，用来传递异步操作的消息。
+
+Promise对象有以下两个特点:
+
+* 对象的状态不受外界影响。Promise对象代表一个异步操作，有三种状态：Pending（进行中）、Resolved（已完成，又称Fulfilled）和Rejected（已失败）。只有异步操作的结果，可以决定当前是哪一种状态，任何其他操作都无法改变这个状态。
+* 一旦状态改变，就不会再变，任何时候都可以得到这个结果。Promise对象的状态改变，只有两种可能：从Pending变为Resolved和从Pending变为Rejected。只要这两种情况发生，状态就凝固了，不会再变了，会一直保持这个结果。就算改变已经发生了，你再对Promise对象添加回调函数，也会立即得到这个结果。这与事件（Event）完全不同，事件的特点是，如果你错过了它，再去监听，是得不到结果的。
+
+有了Promise对象，就可以将异步操作以同步操作的流程表达出来，避免了层层嵌套的回调函数。此外，Promise对象提供统一的接口，使得控制异步操作更加容易。
+
+Promise也有一些缺点:
+
+* 首先，无法取消Promise，一旦新建它就会立即执行，无法中途取消。
+* 其次，如果不设置回调函数，Promise内部抛出的错误，不会反应到外部。
+* 第三，当处于Pending状态时，无法得知目前进展到哪一个阶段（刚刚开始还是即将完成）。
+
+### 基本用法
+```
+//创建promise
+var promise = new Promise(function(resolve, reject) {
+    // 进行一些异步或耗时操作
+    if ( /*如果成功 */ ) {
+        resolve("Stuff worked!");
+    } else {
+        reject(Error("It broke"));
+    }
+});
+//绑定处理程序
+promise.then(function(result) {
+    //promise成功的话会执行这里
+    document.write(result); // "Stuff worked!"
+}, function(err) {
+    //promise失败会执行这里
+    document.write(err); // Error: "It broke"
+});
+```
+
+* resolve函数的作用是，将Promise对象的状态从“未完成”变为“成功”（即从Pending变为Resolved），在异步操作成功时调用，并将异步操作的结果，作为参数传递出去；
+* reject函数的作用是，将Promise对象的状态从“未完成”变为“失败”（即从Pending变为Rejected），在异步操作失败时调用，并将异步操作报出的错误，作为参数传递出去。
+
+## class
+ES6提供了更接近传统语言的写法，引入了Class（类）这个概念，作为对象的模板。通过class关键字，可以定义类。
+
+### 基本语法
+```
+//定义类
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  toString() {
+    return '('+this.x+', '+this.y+')';
+  }
+}
+
+var point = new Point(2, 3);
+```
+
+### 继承
+Class之间可以通过extends关键字，实现继承。
+
+```
+class ColorPoint extends Point {
+  constructor(x, y, color) {
+    this.color = color; // ReferenceError
+    super(x, y);
+    this.color = color; // 正确
+  }
+}
+```
+
+### 取值函数（getter）和存值函数（setter）
+```
+class MyClass {
+  get prop() {
+    return 'getter';
+  }
+  set prop(value) {
+    document.write('setter: '+value);
+  }
+}
+ 
+let inst = new MyClass();
+ 
+inst.prop = 123;
+// setter: 123
+ 
+inst.prop
+// 'getter'
+```
+
+### 静态方法
+类相当于实例的原型，所有在类中定义的方法，都会被实例继承。如果在一个方法前，加上static关键字，就表示该方法不会被实例继承，而是直接通过类来调用，这就称为“静态方法”。
+
+```
+class Foo {
+  static classMethod() {
+    return 'hello';
+  }
+}
+Foo.classMethod() // 'hello'
+var foo = new Foo();
+foo.classMethod()
+// TypeError: undefined is not a function
+```
+
+## Module
+### export
+export命令用于用户自定义模块，规定对外接口；
+
+### import
+import命令用于输入其他模块提供的功能，同时创造命名空间（namespace），防止函数名冲突。
+
+### module
+module命令可以取代import语句，达到整体输入模块的作用。
+
+### export default
+为加载模块指定默认输出，使用export default命令。
